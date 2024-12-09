e636c66e290cb706410d1a53b528ce4a31a25cbc
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-12-09 22:40:07 +0700

    Add health check

--- lua/nvim-ducky/display.lua
+++ lua/nvim-ducky/display.lua
@@ -2,18 +2,39 @@ local display = {}
 local n = require("nui-components")
 
 local renderer = n.create_renderer({
-	width = 80,
-	height = 40,
+	width = 10,
+	height = 20,
 })
 
 function display:new(obj)
 	vim.print(obj.focus_node)
 
-	renderer:render(n.paragraph({
-		lines = "nui.components",
-		align = "center",
-		is_focusable = false,
-	}))
+	local body = function()
+		return n.rows(
+			n.columns(
+				{ flex = 0 },
+				n.text_input({
+					autofocus = true,
+					flex = 1,
+					max_lines = 1,
+				}),
+				n.gap(1),
+				n.button({
+					label = "Send",
+					padding = {
+						top = 1,
+					},
+				})
+			),
+			n.paragraph({
+				lines = "nui.components",
+				align = "center",
+				is_focusable = false,
+			})
+		)
+	end
+
+	renderer:render(body)
 end
 
 return display
