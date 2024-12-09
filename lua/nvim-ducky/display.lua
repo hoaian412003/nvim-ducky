bebd1338332888ce72d8c5f36280ad9962494ded
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-12-10 00:05:03 +0700

    Update

--- lua/nvim-ducky/display.lua
+++ lua/nvim-ducky/display.lua
@@ -8,8 +8,6 @@ local renderer = nui.create_renderer({
 })
 
 function display:new(obj)
-	vim.print(table.unpack(nodes:render(obj.focus_node)))
-
 	local body = function()
 		nui.columns(table.unpack(nodes:render(obj.focus_node)))
 	end
