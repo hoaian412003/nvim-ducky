15d6ccd838941b51f1d10e5a477154202daa6605
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-12-09 23:29:23 +0700

    Update

--- lua/nvim-ducky/nodes.lua
+++ lua/nvim-ducky/nodes.lua
@@ -3,9 +3,7 @@ local nodes = {}
 
 function nodes:render(current_node)
 	local result = {}
-	vim.print("Current node is: ", current_node)
 	while current_node ~= nil do
-		vim.print(current_node.index, current_node)
 		result[current_node.index] = node:render(current_node)
 		current_node = current_node.next
 	end
