44a4e7853f7bdc1c2511453b10e968d4e294911d
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-12-11 17:16:55 +0700

    Update

--- lua/nvim-ducky/display.lua
+++ lua/nvim-ducky/display.lua
@@ -62,6 +62,12 @@ function display:fill_buffer(buffer, current_node, config)
 	for k, node in ipairs(nodes) do
 		local hl_group = "Navbuddy" .. navic.adapt_lsp_num_to_str(node.kind)
 		vim.api.nvim_buf_add_highlight(buffer, ns, hl_group, k - 1, 0, -1)
+		if node.index == current_node.index then
+			vim.api.nvim_win_set_cursor(buffer.winid, {
+				row = k,
+				col = 0,
+			})
+		end
 	end
 end
 
